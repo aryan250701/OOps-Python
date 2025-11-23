bff3485450de03f2d5b929d790c65865916085b8
Aryan <aryan.dash2507@gmail.com>
2025-11-23 14:31:11 +0530

    private and protected

--- script_2.py
+++ script_2.py
@@ -2,7 +2,7 @@ class Dog:
     #Class is analogus to a blueprint (eg String, List, Dictionary etc)
     # Method to make the dog bark
     # Methods are functions defined inside a class eg(.upper(), .append() etc)
-    def __init__(self, name, breed, owner):
+    def __init__(self, name:str, breed, owner):
         self.name = name
         self.breed = breed
         self.owner = owner
@@ -10,7 +10,7 @@ class Dog:
     
     def bark(self):
         print("woof woof")
-
+ 
 class Owner:
     def __init__(self,name, address, contact_number):
         self.name = name
@@ -26,7 +26,7 @@ class Owner:
 name = "Aryan"
 # this is basically short hand notation for 
 # name = str("Aryan")
-print(name.upper())  # Calling the .upper() method on the string object 'name'
+# print(name.upper())  # Calling the .upper() method on the string object 'name'
 
 # Summary
 
@@ -43,5 +43,21 @@ print(name.upper())  # Calling the .upper() method on the string object 'name'
 # print(dog_1.name)  # Accessing the name attribute of dog_1
 
 owner_1 = Owner("Alice", "123 Main St", "555-1234")
-dog_1 = Dog("Buddy", "Golden Retriever", owner_1)
+dog_1 = Dog(1, "Golden Retriever", owner_1)
 print(dog_1.owner.name)  # Accessing the owner's name through the dog_1 object
+
+## Making an Object private
+## Method _1: Single underscore prefix
+## Method _2: Double underscore prefix
+class Cat:
+    def __init__(self, cat_name, age, password, secret_code):
+        self.cat_name = cat_name
+        self.age = age
+        self._password = password  # single underscore prefix (can still be accessed but indicates it's private)
+        self.__secret_code = secret_code  # double underscore prefix (makes it harder to access from outside)
+        pass
+    
+cat_1 = Cat("snowy",3,"1234","ABC")
+print(cat_1.cat_name)  # Accessing public attribute
+print(cat_1._password)  # Accessing "private" attribute with single underscore (not recommended)
+# print(cat_1.__secret_code)  # This will raise an AttributeError
